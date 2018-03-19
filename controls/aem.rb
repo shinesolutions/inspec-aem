9c3fec4388c2e197f0d5bf13c29976a4be39e464
Cliffano Subagio <cliffano@gmail.com>
2018-03-19 17:23:38 +1100

    Clean up lint violations.

--- controls/aem.rb
+++ controls/aem.rb
@@ -1,5 +1,6 @@
 control 'aem-crxde-enabled' do
   impact 1.0
+  title 'Check AEM CRXDE is enabled'
   desc 'Check AEM CRXDE is enabled'
   describe aem do
     it { should have_crxde_enabled }
@@ -8,6 +9,7 @@ end
 
 control 'aem-crxde-disabled' do
   impact 1.0
+  title 'Check AEM CRXDE is disabled'
   desc 'Check AEM CRXDE is disabled'
   describe aem do
     it { should have_crxde_disabled }
